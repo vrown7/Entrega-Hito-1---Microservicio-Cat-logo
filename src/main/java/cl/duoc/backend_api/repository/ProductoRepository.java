29b7c1d0aa13c05751ed219009e6e85ecbe16b8b
Nicolas <ni.ortegat@duocuc.cl>
2026-04-23 00:39:51 -0400

    Delete src directory

--- src/main/java/cl/duoc/backend_api/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.backend_api.repository;
-
-import cl.duoc.backend_api.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
